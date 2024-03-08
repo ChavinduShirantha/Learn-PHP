ffae8843778c312f63b1defd47a575322cea243c
ChavinduShirantha <chavindushirantha@gmail.com>
2024-03-08 13:10:19 +0530

    How to use variables in php

--- site.php
+++ site.php
@@ -7,10 +7,12 @@
   <body>
 
     <?php
-      echo "There once was a man named George <br>";
-      echo "He was 70 years old <br>";
-      echo "He really liked the name George <br>";
-      echo "But didn't like being 70 <br>";
+      $characterName = "George" ;
+      $characterAge = 75 ;
+      echo "There once was a man named $characterName <br>";
+      echo "He was $characterAge years old <br>";
+      echo "He really liked the name $characterName <br>";
+      echo "But didn't like being $characterAge <br>";
     ?>
 
   </body>
